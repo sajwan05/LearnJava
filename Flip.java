4baaa84d552293acad67b696410d7ce12a1a24c7
Sajwan05 <surajsajwan328@gmail.com>
2023-07-28 14:43:52 +0530

    Modified Flip.java file

--- Flip.java
+++ Flip.java
@@ -1,10 +1,6 @@
 public class Flip {
     public static void main(String[] args){
-        if(Math.random()<0.5){
-            System.out.println("Head");
-        }
-        else{
-            System.out.println("Tail");
-        }
+        if(Math.random()<0.5) System.out.println("Head");
+        else                  System.out.println("Tail");
     }
 }
