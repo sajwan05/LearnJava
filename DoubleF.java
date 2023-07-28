5db3094ef844b182f0b0f93571875eba6818b65a
Sajwan05 <surajsajwan328@gmail.com>
2023-07-28 21:41:28 +0530

    made a program which give true if a number is strictly between 0 and 1

---
+++ DoubleF.java
@@ -0,0 +1,11 @@
+//  Write a code fragment that prints true if the double variables x and y are 
+// both strictly between 0 and 1, and false otherwise.
+public class DoubleF {
+    public static void main(String[] args){
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        boolean isBetween;
+        isBetween = (x>0 && x<1) && (y>0 && y<1);
+        System.out.println(isBetween);
+    }
+}
